eb41d8bb8de60ae8b5cf8be88be0bebcdb9b7417
Glendon Philipp Baculio <gpbaculio@gmail.com>
2020-04-09 04:10:45 +0800

    ui finished

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import styled from 'styled-components/native'
+import UserInfo from './components/UserInfoContainer';
 
 const App = () => {
   const [isLoadingComplete, setIsLoadingComplete] = useState(false);
@@ -15,16 +16,40 @@ const App = () => {
             </AvatarContainer>
             <UserInfoContainer>
               <UserInfoHeaderContainer>
-                <Username>Shela Joy Huiso</Username>
+                <Username>Sharmaine Red</Username>
                 <UserInfoSubheaderContainer>
                   <UserInfoSubheader>Female</UserInfoSubheader>
-                  <HorizontalDivider />
+                  <VerticalDivider />
                   <UserInfoSubheader>1 pet</UserInfoSubheader>
                 </UserInfoSubheaderContainer>
               </UserInfoHeaderContainer>
+              <UserInfo label={'Phone Number:'}  value={'0928 765 4321'}/>
+              <UserInfo label={'Email Address:'}  value={'gpbaculio@gmail.com'}/>
+              <UserInfo label={'Address:'}  value={'Northern Mindanao, Philippines'}/>
             </UserInfoContainer>
           </Body>
         </BodyContainer>
+        <BottomContainer>
+          <BottomHeaderContainer>
+            <BottomHeader bgc={'purple'}>asd</BottomHeader>
+            <BottomHeader bgc={'orange'}>asd</BottomHeader>
+          </BottomHeaderContainer>
+          <PetsContainer>
+            <PetContainer>
+              <PetAvatar source={{ uri: 'https://randomuser.me/api/portraits/women/2.jpg' }} />
+              <PetInfoContainer>
+                <PetName>
+                  Jake
+                </PetName>
+                <PetInfo>
+                  <PetInfoValue>Pomeranian</PetInfoValue>
+                  <VerticalDivider />
+                  <PetInfoValue>1 year old</PetInfoValue>
+                </PetInfo>
+              </PetInfoContainer>
+            </PetContainer>
+          </PetsContainer>
+        </BottomContainer>
       </AppBackgroundImg>
     </AppContainer>
   );
@@ -45,7 +70,7 @@ const AppBackgroundImg = styled.ImageBackground`
 `
 
 const Header = styled.View`
-  flex: 0.2;
+  flex: 0.25;
   justify-content: center;
   align-items: center;
 `
@@ -53,7 +78,7 @@ const Header = styled.View`
 const avatarWidth = width * 0.30;
 
 const BodyContainer = styled.View`
-  flex: 0.45;
+  flex: 0.4;
   align-items: center;
   background-color: #f5f5f5;
   position: relative;
@@ -90,10 +115,11 @@ const UserInfoContainer = styled.View`
   justify-content: space-around;
   align-items: center;
   flex: 1;
-  background-color: red;
+  padding-bottom: 8px;
 `
 
 const Username = styled.Text`
+  letter-spacing: 2px;
   font-size:18px;
   font-weight: 700;
   color: gray;
@@ -110,15 +136,80 @@ const UserInfoSubheaderContainer = styled.View`
 `
 
 const UserInfoSubheader = styled.Text`
-  font-size: 16px;
+  font-size: 14px;
   font-weight: 500;
   color: gray;
 `
 
-const HorizontalDivider = styled.View`
-  height: 16px;
+const VerticalDivider = styled.View`
+  height: 14px;
   border-style: solid;
   border-left-width: 2px;
   border-color: gray;
-  margin-horizontal: 8px;
+  margin-horizontal: 12px;
+`
+
+const BottomContainer = styled.View`
+  flex: 0.35
+`
+
+const BottomHeaderContainer = styled.View`
+  flex-direction: row;
+  width: 100%;
+  background-color:yellow;
+`
+
+interface BottomHeaderProps {
+  bgc: string
+}
+
+const BottomHeader = styled.Text<BottomHeaderProps>`
+  padding-vertical: 7px;
+  flex:1;
+  text-align: center;
+  justify-content: center;
+  color: #fff;
+  ${props => props.bgc && `background-color: ${props.bgc}`}
+`
+
+const PetsContainer = styled.View`
+  margin: 16px;
+  flex-direction: column;
+`
+
+const PetContainer = styled.View`
+  shadow-color: black;
+  elevation: 1;
+  shadow-offset: 0 4px;
+  shadow-opacity: 0.5;
+  shadow-radius: 4px;
+  border-radius: 4px;
+  background-color: #fff;
+  padding: 12px;
+  flex-direction: row;
+  align-items:center;
+`
+
+const PetAvatar = styled.Image`
+  width: 50px;
+  height: 50px;
+  border-radius: 25px;
+  margin-right: 12px;
+`
+
+const PetInfoContainer = styled.View`
+  flex-direction: column;
+`
+
+const PetName = styled.Text`
+  font-weight: bold;
+`
+
+const PetInfo = styled.View`
+  flex-direction: row;
+  align-items: center;
+`
+
+const PetInfoValue = styled.Text`
+  color: gray;
 `
---
+++ components/UserInfoContainer.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import styled from 'styled-components/native'
+
+interface InfoContainerProps {
+  label: string
+  value: string
+}
+
+const UserInfo: React.FC<InfoContainerProps> = ({ label, value }) => (
+  <UserInfoContainer>
+    <UserInfoLabel>{label}</UserInfoLabel>
+    <UserInfoValue>{value}</UserInfoValue>
+  </UserInfoContainer>
+)
+
+export default UserInfo
+
+const UserInfoContainer = styled.View`
+  flex-direction: column;
+  align-items:center;
+`
+
+const UserInfoLabel = styled.Text`
+  color: purple;
+  font-size: 14px;
+  font-weight: 700;
+`
+
+const UserInfoValue = styled.Text`
+  color: gray;
+  font-size: 14px;
+  font-weight: 500;
+`
